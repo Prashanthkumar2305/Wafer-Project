cb2aa6bda1477a7488a5d1aadb3b743cfacf4fc5
prashanthkumar k <prashanthpacchin103@gmail.com>
2023-06-14 21:45:57 +0530

    first commit and added raw data

---
+++ wafer_main/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='it's a binary classification problems with using of MLOPS',
+    author='prashanthkumar k',
+    license='MIT',
+)
